28d02d9c4e5410a25f8ae2b540e8807b530b44fa
Joshua Winskill <Josh@Joshuas-MacBook-Pro.local>
2014-09-29 15:33:19 -0700

    Initial Commit

---
+++ iOS_F2_Homework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_F2_Homework
+//
+//  Created by Joshua Winskill on 9/29/14.
+//  Copyright (c) 2014 Joshua Winskill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
